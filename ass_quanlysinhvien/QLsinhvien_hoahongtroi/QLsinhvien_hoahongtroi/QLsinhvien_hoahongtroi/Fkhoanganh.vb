e19b41e59ebb460cb8be08e7a2693a610701e193
hoahongtroi <kans2ngok@gmail.com>
2017-03-13 14:26:22 +0700

    Revert "Add files via upload"
    This reverts commit 68cd3f6dbecf43537a820c23a4ecf491a7764337.

--- ass_quanlysinhvien/QLsinhvien_hoahongtroi/QLsinhvien_hoahongtroi/QLsinhvien_hoahongtroi/Fkhoanganh.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class Fkhoanganh
-
-End Class
--- ass_quanlysinhvien/QLsinhvien_hoahongtroi/QLsinhvien_hoahongtroi/QLsinhvien_hoahongtroi/Fmenu.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class Fmenu
-
-    Private Sub SinhVienToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SinhVienToolStripMenuItem.Click
-        frmsinhvien.Show()
-
-
-    End Sub
-End Class
